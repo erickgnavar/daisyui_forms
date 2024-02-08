0c28a7f4371a79adec83e51065fc8a46af74b4bd
Erick Navarro <erick@navarro.io>
2024-02-07 18:26:25 -0600

    Add missed metadata for publication in hex.pm

--- mix.exs
+++ mix.exs
@@ -6,9 +6,18 @@ defmodule DaisyUIForms.MixProject do
       app: :daisyui_forms,
       version: "0.0.1",
       elixir: "~> 1.11",
+      description: "Helper module to generate form elements with DaisyUI",
+      package: package(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/erickgnavar/daisyui_forms"}
     ]
   end
 
