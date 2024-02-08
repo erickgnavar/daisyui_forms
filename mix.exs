0f7157801de71a67cd19f00609453b208c781cf8
Erick Navarro <erick@navarro.io>
2024-02-07 18:21:07 -0600

    Change version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DaisyUIForms.MixProject do
   def project do
     [
       app: :daisyui_forms,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
